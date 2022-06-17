f5e5e98a048e7098c486985ef0d8f4ada47045da
Rossario Catherine Elfrida <99484551+catherineelfrida@users.noreply.github.com>
2022-06-17 20:55:21 +0700

    Delete Cloud Computing directory

--- Cloud Computing/config/config.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("sequelize");
-const db = new sequelize("db_awmonitor", "root", "", {
-  dialect: "mysql",
-  dialectOptions: {
-    socketPath: `/cloudsql/awmonitor-351813:asia-southeast2:awmmysql`,
-  },
-});
-
-module.exports = db;
